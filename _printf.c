73d0630a62804bdb6feb4f5673cba07d491624b2
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-11-15 00:30:06 +0300

    for the final time

--- _printf.c
+++ _printf.c
@@ -1,40 +1,34 @@
 #include "main.h"
 
 /**
- * _printf - Receives the main string and all the necessary parameters to
- * print a formated string
- * @format: A string containing all the desired characters
- * Return: A total count of the characters printed
+ * _printf - implementation of the inbuilt printf
+ * @format: the format specifier
+ * Return: the formated string
  */
 
 int _printf(const char *format, ...)
 {
-	int printed_chars;
-	conver_t f_list[] = {
-		{"%", print_percent},
-		{"d", print_integer},
-		{"i", print_integer},
-		{"c", print_char},
-		{"s", print_string},
-		{"b", print_binary},
-		{"u", print_unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_HEX},
-		{"S", print_String},
-		{"p", print_pointer},
-		{"r", print_rev},
-		{"R", print_rot13},
-		{NULL, NULL},
-	};
-	va_list arg_list;
+	int printed = 0;
 
-	if (format == NULL)
-		return (-1);
+	va_list args;
 
-	va_start(arg_list, format);
-	printed_chars = format_reciever(format, f_list, arg_list);
-	va_end(arg_list);
-	return (printed_chars);
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			printed = selector(format, args, printed);
+			format++;
+		}
+		else
+		{
+			_putchar(*format);
+			printed++;
+			format++;
+		}
+	}
+	va_end(args);
+	return (printed);
 }
-
